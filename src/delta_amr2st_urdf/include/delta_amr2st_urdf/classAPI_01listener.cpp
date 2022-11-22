340f7b82902aed3fcc078b9ff3cb074708b33470
Tsai,Li-chun <4a237051@stust.edu.tw>
2022-11-22 14:54:07 +0800

    only pub&sub changes class style

---
+++ src/delta_amr2st_urdf/include/delta_amr2st_urdf/classAPI_01listener.cpp
@@ -0,0 +1,73 @@
+/** ******************************************************
+	* @file		classAPI_01listener.cpp
+	* @author	Tsai,Li-chun
+	******************************************************
+**	**/
+
+
+/* System Includes ------------------------------------------*/
+/* System Includes Begin */
+/* System Includes End */
+/* User Includes --------------------------------------------*/
+/* User Includes Begin */
+#include "classAPI_01listener.hpp"
+/* User Includes End */
+
+/* namespace ------------------------------------------------*/
+/* namespace Begin */
+/* namespace End */
+
+
+/* Define ---------------------------------------------------*/
+/* Define Begin */
+/* Define End */
+
+
+/* Typedef --------------------------------------------------*/
+/* Typedef Begin */
+/* Typedef End */
+
+
+/* Class --------------------------------------------------*/
+/* Class Begin */
+/* Class End */
+
+
+/* Variables ------------------------------------------------*/
+/* Variables Begin */
+/* Variables End */
+
+
+/* Function -------------------------------------------------*/
+/* Function Begin */
+/* Function End */
+
+
+
+/* ---------------------------------------------------------*/
+/* ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ Program ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ ---------------------------*/
+/* ---------------------------------------------------------*/
+/* Program Begin */
+
+listener::listener()
+{
+	sub = this->subscribe("chatter",10,&listener::chatterCallback, this);
+}
+
+listener::~listener()
+{
+	ros::shutdown();
+}
+
+void listener::chatterCallback(const std_msgs::String& msg)
+{
+	ROS_INFO("subscribe: %s", msg.data.c_str());
+}
+
+/* Program End */
+/* ---------------------------------------------------------*/
+/* ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ Program ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ ---------------------------*/
+/* ---------------------------------------------------------*/
+
+
+/* ***** END OF classAPI_01listener.cpp ***** */
---
+++ src/delta_amr2st_urdf/include/delta_amr2st_urdf/classAPI_01talker.cpp
@@ -0,0 +1,72 @@
+/** ******************************************************
+	* @file		classAPI_01talker.cpp
+	* @author	Tsai,Li-chun
+	******************************************************
+**	**/
+
+
+/* System Includes ------------------------------------------*/
+/* System Includes Begin */
+/* System Includes End */
+/* User Includes --------------------------------------------*/
+/* User Includes Begin */
+#include "classAPI_01talker.hpp"
+/* User Includes End */
+
+/* namespace ------------------------------------------------*/
+/* namespace Begin */
+/* namespace End */
+
+
+/* Define ---------------------------------------------------*/
+/* Define Begin */
+/* Define End */
+
+
+/* Typedef --------------------------------------------------*/
+/* Typedef Begin */
+/* Typedef End */
+
+
+/* Class --------------------------------------------------*/
+/* Class Begin */
+/* Class End */
+
+
+/* Variables ------------------------------------------------*/
+/* Variables Begin */
+/* Variables End */
+
+
+/* Function -------------------------------------------------*/
+/* Function Begin */
+/* Function End */
+
+
+
+/* ---------------------------------------------------------*/
+/* ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ Program ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ ---------------------------*/
+/* ---------------------------------------------------------*/
+/* Program Begin */
+
+talker::talker()
+{
+	pub = this->advertise<std_msgs::String>("chatter", 10);
+}
+talker::~talker()
+{
+	this->shutdown();
+}
+
+void talker::publish(std_msgs::String msg)
+{
+	pub.publish(msg);
+}
+
+/* Program End */
+/* ---------------------------------------------------------*/
+/* ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ Program ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ ---------------------------*/
+/* ---------------------------------------------------------*/
+
+
+/* ***** END OF classAPI_01talker.cpp ***** */
