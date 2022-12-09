50a54b9123dbd466697ce75cde53a2faebf6a991
Tsai,Li-chun <4a237051@stust.edu.tw>
2022-12-09 17:11:00 +0800

    add modbus encoder to wheel mm

---
+++ src/delta_amr2st_urdf/src/node_handle_namespace.cpp
@@ -0,0 +1,77 @@
+/** ******************************************************
+	* @file		node_handle_namespace.cpp
+	* @author	Tsai,Li-chun
+	******************************************************
+**	**/
+
+
+/* System Includes ------------------------------------------*/
+/* System Includes Begin */
+/* System Includes End */
+/* User Includes --------------------------------------------*/
+/* User Includes Begin */
+#include <ros/ros.h>
+/* User Includes End */
+
+/* namespace ------------------------------------------------*/
+/* namespace Begin */
+/* namespace End */
+
+
+/* Define ---------------------------------------------------*/
+/* Define Begin */
+/* Define End */
+
+
+/* Typedef --------------------------------------------------*/
+/* Typedef Begin */
+/* Typedef End */
+
+
+/* Class --------------------------------------------------*/
+/* Class Begin */
+/* Class End */
+
+
+/* Variables ------------------------------------------------*/
+/* Variables Begin */
+/* Variables End */
+
+
+/* Function -------------------------------------------------*/
+/* Function Begin */
+/* Function End */
+
+
+
+/* ---------------------------------------------------------*/
+/* ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ Program ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ ---------------------------*/
+/* ---------------------------------------------------------*/
+/* Program Begin */
+
+/** * @brief  Program entry point.
+	* @param argc(int) Number of input parameters
+ 	* @param argv(int) input parameters
+ 	* @return (int) Program Error.
+**	**/
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "node_handle_namespace");
+	ros::NodeHandle nh0;
+	ros::NodeHandle nh1("LJ_namespace1");
+	ros::NodeHandle nh2("LJ_namespace2");
+	ros::NodeHandle nh3("~");
+	// ros::Subscriber sub = nh0.subscribe("topic0", 10, inline{});
+	ros::spin();
+
+	/* main quit */
+	return 0;
+}
+
+/* Program End */
+/* ---------------------------------------------------------*/
+/* ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ Program ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ ---------------------------*/
+/* ---------------------------------------------------------*/
+
+
+/* ***** END OF node_handle_namespace.cpp ***** */
--- src/modbus_api/src/modbus_test.cpp
+++ src/modbus_api/src/modbus_test.cpp
@@ -9,6 +9,7 @@
 /* System Includes Begin */
 #include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
 /* System Includes End */
 /* User Includes --------------------------------------------*/
 /* User Includes Begin */
@@ -60,49 +61,94 @@
 **	**/
 int main(int argc, char **argv)
 {
-    modbus_t *ctx;
-    // uint8_t raw_req[] = { 0x01, 06, 0x02, 0x87, 0x01, 0x23 };
-    uint16_t raw_req[] = { 0, 48, 0, 0, 0x0000, 0x0, 0, 1000, 0, 2500, 0, 1000, 0, 1 };
-    // uint16_t raw_req[] = { 0, 48, 0, 0, 0xFFFF, 0xFE0C, 0, 1000, 0, 2500, 0, 1000, 0, 1 };
-    // uint16_t raw_req[] = { 0, 48, 0, 0, 0xFFFF, 0xFC18, 0, 1000, 0, 2500, 0, 1000, 0, 1 };
-    // uint16_t raw_req[] = { 0, 0 };
-    uint16_t rsp[64];
-    int req_length,i;
+    const double enc_to_mm = 0.000561414233148148;
+    modbus_t *MD_L, *MD_R;
+    uint16_t raw_req_SON[]={ 0,  1 };
+    uint16_t raw_req_Z[] = { 0,  0, 0, 0, 0,    0, 0, 2000, 0, 2000, 0, 1000, 0, 1 };
+    uint16_t raw_req_F[] = { 0, 48, 0, 0, 0, 1000, 0, 2000, 0, 2000, 0, 1000, 0, 1 };
+    uint16_t raw_req_B[] = { 0, 48, 0, 0, 0xFFFF, 0xFE0C, 0, 1000, 0, 2500, 0, 1000, 0, 1 };
+    uint16_t Actual_Position[2] = {0};
+    int wheel_L_mm,wheel_R_mm;
+    int wheel_L_enc,wheel_R_enc;
+    int req_length,i,j;
 
 	ros::init(argc, argv, "modbus_test");
 
-    ctx = modbus_new_rtu("/dev/ttyS0", 115200, 'E', 8, 1);
-    if (ctx == NULL)
+    MD_L = modbus_new_rtu("/dev/ttyS0", 115200, 'E', 8, 1);
+    if (MD_L == NULL)
     {
         fprintf(stderr, "Unable to create the libmodbus context\n");
         return -1;
     }
-
-    modbus_set_slave(ctx, 1);
-
-    if (modbus_connect(ctx) == -1)
+    modbus_set_slave(MD_L, 1);
+    if (modbus_connect(MD_L) == -1)
     {
         fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-        modbus_free(ctx);
+        modbus_free(MD_L);
         return -1;
     }
-    else
+    else printf("MD_L OK\n");
+
+    MD_R = modbus_new_rtu("/dev/ttyS0", 115200, 'E', 8, 1);
+    if (MD_R == NULL)
     {
-        printf("OK\n");
+        fprintf(stderr, "Unable to create the libmodbus context\n");
+        return -1;
+    }
+    modbus_set_slave(MD_R, 2);
+    if (modbus_connect(MD_R) == -1)
+    {
+        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+        modbus_free(MD_R);
+        return -1;
+    }
+    else printf("MD_R OK\n");
+
+
+    while(1)
+    {
+        req_length = modbus_read_registers(MD_L, 154, 2, Actual_Position);
+        printf("%02d, %04x %04x  |  ", req_length, Actual_Position[0], Actual_Position[1]);
+        wheel_L_enc = 0;
+        wheel_L_enc = (wheel_L_enc|Actual_Position[0])<<16;
+        wheel_L_enc |= Actual_Position[1];
+        for(j=0; j<15; j++)for(i=0; i<65535; i++){}
+        req_length = modbus_read_registers(MD_R, 154, 2, Actual_Position);
+        printf("%02d, %04x %04x\n", req_length, Actual_Position[0], Actual_Position[1]);
+        wheel_R_enc = 0;
+        wheel_R_enc = (wheel_R_enc|Actual_Position[0])<<16;
+        wheel_R_enc |= Actual_Position[1];
+
+        printf("%10d, %10d\n", wheel_L_enc, wheel_R_enc);
+        printf("%10.2lf, %10.2lf\n", enc_to_mm*wheel_L_enc, enc_to_mm*wheel_R_enc);
+
+
+
+        sleep(1);
     }
 
-    // req_length = modbus_write_registers(ctx, 0x007C, 2, raw_req);
-    req_length = modbus_write_registers(ctx, 0x005A, 0x000E, raw_req);
-    printf("%d\n", req_length);
-    // req_length = modbus_read_registers(ctx, 646, 2, rsp);
+    // req_length = modbus_write_registers(MD_L, 0x007C, 2, raw_req_SON);
+    // req_length = modbus_write_register(MD_L, 0x007D, 0);
+    // printf("%d\n", req_length);
+    // sleep(1);
+
+    // req_length = modbus_write_registers(MD_L, 0x005A, 0x000E, raw_req_B);
+    // printf("%d\n", req_length);
+    // sleep(1);
+
+    // req_length = modbus_write_registers(MD_L, 0x005A, 0x000E, raw_req_Z);
+    // printf("%d\n", req_length);
+    // sleep(1);
+
+    // req_length = modbus_read_registers(MD_L, 646, 2, rsp);
     // printf("%d\n", req_length);
     // if( req_length >0 )
     //     for(i=0; i<req_length; i++)
     //         printf("rsp[%d]= 0x%04X\n", i, rsp[i]);
 
 
-    modbus_close(ctx);
-    modbus_free(ctx);
+    modbus_close(MD_L);
+    modbus_free(MD_L);
 	
 	ros::shutdown();
 	/* main quit */
