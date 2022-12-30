82ee0f67c6ef61a0c6171938e52e03859439d696
Tsai,Li-chun <4a237051@stust.edu.tw>
2022-12-30 17:44:11 +0800

    add odometry_transform pkg(OnGoing) and odometry_transform_msg(OnGoing)

---
+++ src/odometry_transform/src/main_tf_odometry.cpp
@@ -0,0 +1,84 @@
+/** ******************************************************
+	* @file		main_tf_odometry.cpp
+	* @author	Tsai,Li-chun
+	******************************************************
+**	**/
+
+
+/* System Includes ------------------------------------------*/
+/* System Includes Begin */
+/* System Includes End */
+/* User Includes --------------------------------------------*/
+/* User Includes Begin */
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+/* User Includes End */
+
+/* namespace ------------------------------------------------*/
+/* namespace Begin */
+/* namespace End */
+
+
+/* Define ---------------------------------------------------*/
+/* Define Begin */
+/* Define End */
+
+
+/* Typedef --------------------------------------------------*/
+/* Typedef Begin */
+/* Typedef End */
+
+
+/* Class --------------------------------------------------*/
+/* Class Begin */
+/* Class End */
+
+
+/* Variables ------------------------------------------------*/
+/* Variables Begin */
+/* Variables End */
+
+
+/* Function -------------------------------------------------*/
+/* Function Begin */
+
+void sub_enc_callback(const std_msgs::String& str);
+
+/* Function End */
+
+
+
+/* ---------------------------------------------------------*/
+/* ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ Program ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ ---------------------------*/
+/* ---------------------------------------------------------*/
+/* Program Begin */
+
+/** * @brief  Program entry point.
+	* @param argc(int) Number of input parameters
+ 	* @param argv(int) input parameters
+ 	* @return (int) Program Error.
+**	**/
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "main_tf_odometry");
+	ros::NodeHandle nh;
+	ros::Subscriber sub_enc = nh.subscribe("/motor_encoder", 100, sub_enc_callback);
+
+	ros::spin();
+
+	/* main quit */
+	return 0;
+}
+
+void sub_enc_callback(const std_msgs::String& str)
+{
+	
+}
+
+/* Program End */
+/* ---------------------------------------------------------*/
+/* ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ Program ⇧⇧⇧⇧⇧⇧⇧⇧⇧⇧ ---------------------------*/
+/* ---------------------------------------------------------*/
+
+
+/* ***** END OF main_tf_odometry.cpp ***** */
