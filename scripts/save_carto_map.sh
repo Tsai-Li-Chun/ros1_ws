ab9ff81f055215f0babdbd7c5976beb5a4c80811
Ender024 <michael8805@gmail.com>
2024-05-23 18:07:05 +0800

    discard package for ROS2

--- scripts/save_carto_map.sh
+++
@@ -1,5 +0,0 @@
-rosservice call /finish_trajectory "trajectory_id: 0" &
-sleep 3
-rosservice call /write_state "filename: /home/deltaagv/ros1_ws/map/new_map.pbstream" &
-sleep 3
-rosrun map_server map_saver_cartographer -f /home/deltaagv/ros1_ws/map/231127_delta_8f
